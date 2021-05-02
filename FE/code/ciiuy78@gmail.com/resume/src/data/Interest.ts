9233713be5c762b4f5aa468615f0c49a7de6a2e5
hopehappy92 <hopehappy92@naver.com>
2021-05-02 17:45:49 +0900

    feat : add interest

---
+++ FE/code/ciiuy78@gmail.com/resume/src/data/Interest.ts
@@ -0,0 +1,32 @@
+import { IInterest } from "../model/interest";
+
+const InterestData: IInterest[] = [
+  {
+    title: "RoadMap",
+    link: "https://www.notion.so/ROAD-MAP-8290b610977941c6a87071eb888ee970",
+  },
+  {
+    title: "Webpack",
+    link: "https://www.notion.so/Webpack-568c659c6fb04a208ce0b17601cd7ab7",
+  },
+  {
+    title: "Husky & Lint-Staged",
+    link:
+      "https://www.notion.so/Husky-Lint-staged-7e125273240b4d9e831b25fa2764c512",
+  },
+  {
+    title: "NPM CLI Version Control",
+    link:
+      "https://www.notion.so/NPM-CLI-Version-e27fa38ba7f745fe9cabc8ac0ae58f51",
+  },
+  {
+    title: "GraphQL & Apollo",
+    link: "https://www.notion.so/GraphQL-21637701e1c0455f99519079657fce69",
+  },
+  {
+    title: "Accessibility",
+    link: "https://www.notion.so/feat-NULI-99555401eaaf434fb4c5047ace085149",
+  },
+];
+
+export { InterestData };
